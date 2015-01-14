139128eb4b1631939b038a81aa20dd1cacc34475
IvanSobolevsky <superspam@tut.by>
2015-01-14 11:13:31 +0300

    sad asd asd

---
+++ src/test/java/by/onliner/BrowserTest.java
@@ -0,0 +1,30 @@
+package by.onliner;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class BrowserTest {
+
+    @Test
+    public void browserTest() throws IOException{
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://google.com");
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("fire-screenshot.png"));
+    }
+
+    @Test
+    public void browserChromeTest() throws IOException{
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://tut.by");
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("chrome-screenshot.png"));
+    }
+}
